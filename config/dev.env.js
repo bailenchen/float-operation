7570a099cb500fc7978e2248d39efbb094fa0fd0
yang <yarui@qq.com>
2019-04-15 18:21:18 +0800

    调整开发分支dev地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost/"',
+  BASE_API: '"http://192.168.1.100/snowerp/php/index.php/"',
 })
