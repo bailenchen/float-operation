cbdae83a2161697cb385c15a8b7cd94ab3332d8d
yang <yarui@qq.com>
2019-06-29 10:02:01 +0800

    日志评论和任务评论 通过xss 解决 评论html 变为执行不展示的问题

--- src/views/OAManagement/journal/journalCell.vue
+++ src/views/OAManagement/journal/journalCell.vue
@@ -219,6 +219,7 @@
   </div>
 </template>
 <script type="text/javascript">
+import xss from 'xss'
 import emoji from '@/components/emoji'
 // API
 import { journalSetread } from '@/api/oamanagement/journal'
@@ -445,7 +446,7 @@ export default {
           typeId: item.typeId,
           mainId: item.mainId == 0 ? item.commentId : item.mainId,
           type: 2,
-          content: this.childCommentsTextarea
+          content: xss(this.childCommentsTextarea)
         })
           .then(res => {
             this.childCommentsPopover = false
@@ -478,7 +479,7 @@ export default {
         setCommentAPI({
           typeId: this.showWorkbench ? this.data.actionId : this.data.logId,
           type: 2, // 1 任务 2 日志
-          content: this.commentsTextarea
+          content: xss(this.commentsTextarea)
         })
           .then(res => {
             // 插入一条数据
