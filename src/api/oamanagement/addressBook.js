fb6afa9709731f23072510f35ff6a0eeb9a9d873
yaoxinke <yaohuiofe@163.com>
2019-09-27 16:40:22 +0800

    .

--- src/api/oamanagement/addressBook.js
+++ src/api/oamanagement/addressBook.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 审批类型列表
+// 通讯录列表
 export function addresslist(data) {
   return request({
     url: 'system/user/queryListName',
@@ -13,6 +13,15 @@ export function addresslist(data) {
 export function queryListNameByDept(data) {
   return request({
     url: 'system/user/queryListNameByDept',
+    method: 'post',
+    data: data
+  })
+}
+
+// 通讯录关注状态切换
+export function toggleAttention(data) {
+  return request({
+    url: 'system/user/attention',
     method: 'post',
     data: data
   })
