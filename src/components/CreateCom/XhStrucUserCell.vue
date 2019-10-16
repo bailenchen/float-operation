9424b1a0e98aab8e95883baa3507efbacbf37684
yang <yarui@qq.com>
2019-10-16 18:45:01 +0800

    部门组件细节调整

--- src/components/CreateCom/XhStrucUserCell.vue
+++ src/components/CreateCom/XhStrucUserCell.vue
@@ -3,24 +3,29 @@
     :popover-display="'block'"
     :dep-checked-data="dataStrucs"
     :user-checked-data="dataUsers"
-    @popoverSubmit="popoverSubmit">
+    @popoverSubmit="popoverSubmit"
+    @show="popoverShow">
     <div
       v-if="!$slots.reference"
       slot="membersDep">
       <flexbox
+        :class="['is_valid', { 'is_focus': showPopover }]"
         wrap="wrap"
-        class="structure-container">
+        class="structure-container"
+        @click.native="focusClick">
         <div
-          v-for="(item, index) in dataUsers"
-          :key="'user'+index"
-          class="user-item">{{ item.name ? item.name : item.realname }}
+          v-for="(item, index) in showDataValue"
+          :key="index"
+          class="user-item"
+          @click.stop="deleteItem(item, index)">{{ item.name || item.realname }}
+          <i class="delete-icon el-icon-close"/>
         </div>
+        <i v-if="allData.length > max" class="el-icon-more" />
+        <i
+          :class="['el-icon-arrow-up', { 'is-reverse' : showPopover}]"/>
         <div
-          v-for="(item, index) in dataStrucs"
-          :key="'struc'+index"
-          class="user-item">{{ item.name }}
-        </div>
-        <div class="add-item">+添加</div>
+          v-if="allData.length == 0"
+          class="add-item">{{ placeholder }}</div>
       </flexbox>
     </div>
     <slot
@@ -40,6 +45,10 @@ export default {
   },
   props: {
     // 员工和 部门
+    placeholder: {
+      type: String,
+      default: '请选择'
+    },
     users: {
       type: Array,
       default: () => {
@@ -61,6 +70,10 @@ export default {
         }
       }
     },
+    max: {
+      type: Number,
+      default: 2
+    },
     /** 索引值 用于更新数据 */
     index: Number,
     /** 包含数据源 */
@@ -68,11 +81,25 @@ export default {
   },
   data() {
     return {
+      showPopover: false,
       dataUsers: [], // 关联的时候展示name 编辑的时候展示realname
       dataStrucs: []
     }
   },
-  computed: {},
+  computed: {
+    showDataValue() {
+      if (this.allData.length > this.max) {
+        return this.allData.slice(0, this.max)
+      }
+      return this.allData
+    },
+
+    allData() {
+      const dataUsers = this.dataUsers || []
+      const dataStrucs = this.dataStrucs || []
+      return dataUsers.concat(dataStrucs)
+    }
+  },
   watch: {
     value: function(val) {
       this.dataUsers = val.users
@@ -100,6 +127,29 @@ export default {
         index: this.index,
         value: { users: users, strucs: strucs }
       })
+    },
+
+    popoverShow(val) {
+      this.showPopover = val
+    },
+
+    /** 删除 */
+    deleteItem(item, index) {
+      if (item.userId) {
+        this.dataUsers.splice(index, 1)
+      } else {
+        this.dataStrucs.splice(index - this.dataUsers.length, 1)
+      }
+
+      this.$emit('value-change', {
+        index: this.index,
+        value: { users: this.dataUsers, strucs: this.dataStrucs }
+      })
+    },
+
+    /** 聚焦动作 */
+    focusClick() {
+      this.$emit('focus')
     }
   }
 }
@@ -111,7 +161,7 @@ export default {
   position: relative;
   border-radius: 3px;
   font-size: 12px;
-  border: 1px solid #ddd;
+  border: 1px solid #e6e6e6;
   color: #333333;
   padding: 0.5px;
   line-height: 15px;
@@ -126,7 +176,7 @@ export default {
   }
   .add-item {
     padding: 5px;
-    color: #2362FB;
+    color: #ddd;
     cursor: pointer;
   }
   .delete-icon {
@@ -136,5 +186,62 @@ export default {
   &:hover {
     border-color: #c0c4cc;
   }
+}
+
+.structure-container.is_disabled {
+  background-color: #f5f7fa;
+  border-color: #e4e7ed;
+  cursor: not-allowed;
+  .user-item {
+    background-color: #f0f4f8ea;
+    color: #c0c4cc;
+    cursor: not-allowed;
+  }
+  .delete-icon {
+    color: #c0c4cc;
+    cursor: not-allowed;
+  }
+  .add-item {
+    color: #c0c4cc;
+    cursor: not-allowed;
+  }
+}
+
+.structure-container.is_valid:hover {
+  border-color: #c0c4cc;
+}
+
+.structure-container.is_focus {
+  border-color: $xr-focus-border !important;
+}
+
+.el-icon-more {
+  position: absolute;
+  top: 5px;
+  right: 20px;
+  padding: 6px 10px;
+  font-size: 12px;
+  background-color: #F3F7FF;
+  color: #666;
+  border-radius: $xr-border-radius-base;
+  &:hover {
+    background-color: $xr-color-primary;
+    color: white;
+  }
+}
+
+.el-icon-arrow-up {
+  position: absolute;
+  top: 10px;
+  right: 5px;
+  transition: transform .3s;
+  color: #c0c4cc;
+  font-size: 14px;
+  transition: transform .3s;
+  transform: rotate(180deg);
+  cursor: pointer;
+}
+.el-icon-arrow-up.is-reverse {
+  transform: rotate(0deg);
 }
 </style>
