33799505c526ec3260a7a095998d981a400a4ca4
yang <yarui@qq.com>
2019-12-20 16:07:32 +0800

    修改版本为10.0.1.191220

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const getLocationOrigin = () => {
   return window.location.protocol + '//' + window.location.host + (window.location.port ? ':' + window.location.port : '')
 }
 
-const version = 'V10.0.191207'
+const version = 'V10.0.1.191220'
 
 export default {
   version,
