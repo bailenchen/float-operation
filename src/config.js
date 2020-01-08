2d607b2a565d93ec11960adc2ec756e582e80ac2
yang <yarui@qq.com>
2020-01-08 11:18:30 +0800

    config 修改为 200108

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const getLocationOrigin = () => {
   return window.location.protocol + '//' + window.location.host + (window.location.port ? ':' + window.location.port : '')
 }
 
-const version = 'V10.1.1.20200106'
+const version = 'V10.1.1.20200108'
 
 export default {
   version,
