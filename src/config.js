82a9885e48857ea60f900d63bd9cfc2526330be5
yang <yarui@qq.com>
2020-01-17 18:34:55 +0800

    no message

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const getLocationOrigin = () => {
   return window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
 }
 
-const version = 'V10.1.2.20200117'
 const companyName = '悟空CRM'
+const version = 'V10.1.2.20200117'
 
 export default {
   version,
