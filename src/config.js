04958ebad3a908017e9629f526d746f4a6a80da3
yang <693685409@qq.com>
2020-05-06 17:32:21 +0800

    删除自用系统信息

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const getLocationOrigin = () => {
   return window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
 }
-const wkURL = 'https://s.72crm.com/api/' // 内部私有服务
+const wkURL = '' // 内部私有服务
 const macUrl = 'https://www.5kcrm.com/download/desktop/mac/10/%E6%82%9F%E7%A9%BACRM-10.0.1.dmg'
 const winUrl = 'https://www.5kcrm.com/download/desktop/win/10.1.3/%E6%82%9F%E7%A9%BACRM%20Setup%2010.1.3.exe'
 const iOSUrl = 'https://apps.apple.com/cn/app/%E6%82%9F%E7%A9%BAcrm-10-0/id1489143707'
 const androidUrl = 'https://sj.qq.com/myapp/detail.htm?apkName=com.kakarote.crm10'
 
 /**
- * 悟空暗号
+ *
  * @type {string}
  */
-const cipherStr = 'wk'
+const cipherStr = ''
 
 const companyName = '悟空CRM'
 const version = 'V10.1.5.20200407'
